bb72f8eaa13ca37da0d356332440eac05237f03f
Phuchermes <phucdjhot888@gmail.com>
2026-03-18 23:02:58 +0700

    Initial commit

--- config/api.js
+++ config/api.js
@@ -1,9 +1,5 @@
 // config/api.js
 
-const API_URL = process.env.EXPO_PUBLIC_API_URL;
-
-if (!API_URL) {
-  console.warn("Missing EXPO_PUBLIC_API_URL");
-}
+const API_URL = "https://backend-tlh.onrender.com";
 
 export default API_URL;
